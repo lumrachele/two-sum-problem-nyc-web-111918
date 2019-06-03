9b0901999516255e7901f535e91dfec3e52e40d5
lumrachele <rlum894@gmail.com>
2019-06-02 22:53:43 -0400

    notes for two sum

---
+++ index.js
@@ -0,0 +1,25 @@
+// Given an array of numbers, return every pair of numbers that can be added up to a given target.
+
+// Questions?, edge cases
+// negative numbers
+// whole numbers
+// will there always be an answer?
+// return the pair of numbers in what data type? integer?
+// can the array be empty?
+// zeros?
+
+// give an example of problem and solution
+
+
+function bruteForceTwoSum(array, sum){
+  let pair = []
+  for(let i=0; i<array.length; i++){
+    for(let j=i+1; j<array.length; j++){
+      if(i+j===sum){
+        let matching = [i, j]
+        pair.push(matching)
+      }
+    }
+  }
+  return pair
+}
