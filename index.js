7339bf1d58bada507df7a6dc273cc261e0738854
lumrachele <rlum894@gmail.com>
2019-06-02 22:55:09 -0400

    brute force method

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ function bruteForceTwoSum(array, sum){
   let pair = []
   for(let i=0; i<array.length; i++){
     for(let j=i+1; j<array.length; j++){
-      if(i+j===sum){
-        let matching = [i, j]
+      if(array[i]+array[j]===sum){
+        let matching = [array[i], array[j]]
         pair.push(matching)
       }
     }
